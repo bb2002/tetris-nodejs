c9edcf8265d25ee84d4599d2fc802dde108af790
Ballbot <5252bb@daum.net>
2019-12-22 23:46:00 +0900

    12.22 2019 Index.html

--- vrtetris/index.js
+++ vrtetris/index.js
@@ -1,10 +1,13 @@
 var express = require("express")
 var app = express()
+var path = require("path")
+
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.send("Hello world")
+    res.sendFile(path.join(__dirname, "../public/html/index.html"))
 })
 
 app.listen(9902, () => {
-    console.log("VR Tetris 서버가 기동되었습니다.")
+    console.log("VR Tetris 서버가 기동되었습니다. port : 9902")
 })
